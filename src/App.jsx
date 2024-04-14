3f6882b8fe625a0866375217b37860bef90cc752
Veizh <maximedurville95300@gmail.com>
2024-04-14 17:41:43 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,133 @@
+
+import './App.css';
+import { ClipboardCopy } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react';
+
+function App() {
+  const [latitude, setLatitude] = useState('49.033852');
+  const [longitude, setLongitude] = useState('2.064863');
+  const [rayon, setRayon] = useState('500');
+  let [arrayPlaces,setArrayPlaces]=useState()
+  let dialog=useRef()
+
+  const handleLatitudeChange = (event) => {
+    setLatitude(event.target.value);
+  };
+  const handleRayonChange = (event) => {
+    setRayon(event.target.value);
+  };
+
+  const handleLongitudeChange = (event) => {
+    setLongitude(event.target.value);
+  };
+
+  const handleClick =async  () => {
+    // Ici, vous pouvez ajouter la logique pour traiter les données de latitude et de longitude
+    // par exemple, en les envoyant à un composant parent ou en effectuant une action spécifique
+    const API_KEY = 'AIzaSyD_PRCcTMJ09cWAcrJ-u6RwSMo7dm1-USY';
+
+    // Données de la requête
+    const requestData = {
+      includedTypes: ["bar"],
+      maxResultCount: 20,
+      "rankPreference":"distance",
+      locationRestriction: {
+        circle: {
+          center: {
+            latitude: latitude,
+            longitude:  longitude
+          },
+          radius: rayon
+        }
+      }
+    };
+    
+    // URL de l'endpoint de recherche de l'API Google Places
+    const endpoint = 'https://places.googleapis.com/v1/places:searchNearby';
+    
+    // En-têtes de la requête
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': API_KEY,
+      'X-Goog-FieldMask': 'places.displayName,places.servesBeer,places.goodForWatchingSports,places.servesVegetarianFood,places.servesCocktails,places.outdoorSeating,places.rating,places.formattedAddress,places.nationalPhoneNumber,places.priceLevel'
+    };
+    
+    // Configuration de la requête
+    const options = {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(requestData)
+    };
+    // Effectuer la requête
+   
+     let tmp =await  fetch(endpoint, options)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+    
+        return response.json();
+      })
+      .then(data => {
+        // Traiter la réponse JSON
+        return data
+        // Insérer ici le code pour traiter et afficher les résultats
+      })
+      .catch(error => {
+        console.error('There was a problem with your fetch operation:', error);
+      });
+      setArrayPlaces(tmp)
+     
+      dialog.current&&dialog.current.showModal()
+  };
+ useEffect(()=>{
+  console.log(arrayPlaces);
+ },[arrayPlaces])
+  return (
+    <>
+    <dialog ref={dialog} >
+      <button onClick={ ()=>navigator.clipboard.writeText(JSON.stringify(arrayPlaces,null,2))}>Copy Data<ClipboardCopy strokeWidth={3}/></button>
+      <button onClick={()=>{dialog.current&&dialog.current.close()}}>Close</button>
+    </dialog>
+    <div className='bigcontainer'>
+      <h1>Récuperer les restaurants autour d'un point</h1>
+      <div className="containerform">
+
+      <div className='sub'>
+        <p>
+          Latitude:
+          
+        </p>
+        <input
+            type="text"
+            value={latitude}
+            onChange={handleLatitudeChange}
+            />
+      </div>
+      <div className='sub'>
+       <p>Longitude</p>
+        <input
+            type="text"
+            value={longitude}
+            onChange={handleLongitudeChange}
+            />
+      </div>
+      <div className='sub'>
+      <p>Rayon</p>
+        <input
+            type="number"
+            value={rayon}
+            onChange={handleRayonChange}
+            />
+      </div>
+      </div>
+            
+      <button onClick={handleClick}>Collecte Moi Ca</button>
+    </div>
+    </>
+
+  );
+}
+
+
+export default App
